e41040e7a011c17154517b7d75dee8773a1ad5eb
AdrianRaya <adrian.raya-hernandez@domakaba-external.com>
2024-07-10 17:08:18 +0200

    new version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.alejandro.superheropedia"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
